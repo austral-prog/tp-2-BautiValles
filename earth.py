ee28fb0bc4cd8bad7d66086bc22e0e7498122805
BautiValles <150041744+BautiValles@users.noreply.github.com>
2025-03-28 12:36:42 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,18 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    ToF1 = bool()
+    ToF2 = bool()
+    for i in range(50):#no hay paises con mas de 50 letras
+        if x[i] < y[i]:
+            ToF1 = True
+            ToF2 = False
+            print(f"The result of {x} comes first in the dictionary than {y} is {ToF1}.")
+            print(f"The result of {y} comes first in the dictionary than {x} is {ToF2}.")
+            break
+        elif y[i] < x[i]:
+            ToF1 = True
+            ToF2 = False
+            print(f"The result of {x} comes first in the dictionary than {y} is {ToF1}.")
+            print(f"The result of {y} comes first in the dictionary than {x} is {ToF2}.")
+            break
