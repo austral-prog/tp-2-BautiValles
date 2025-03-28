ac2636a8de15a22f537fdd8da03cf7d043168e66
BautiValles <150041744+BautiValles@users.noreply.github.com>
2025-03-28 12:59:32 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,23 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-    ToF1 = bool()
-    ToF2 = bool()
-    if len(x)<len(y):
-        longitud = len(x)
-    else:
-        longitud = len(y)
-
-    for i in range(longitud):
-        if x[i] < y[i]:
-            ToF1 = True
-            ToF2 = False
-            print(f"The result of {x} comes first in the dictionary than {y} is {ToF1}.")
-            print(f"The result of {y} comes first in the dictionary than {x} is {ToF2}.")
-            break
-        elif y[i] < x[i]:
-            ToF1 = True
-            ToF2 = False
-            print(f"The result of {x} comes first in the dictionary than {y} is {ToF1}.")
-            print(f"The result of {y} comes first in the dictionary than {x} is {ToF2}.")
-            break
+    ToF = str(x<y)
+    ToF2 = str(x>y)
+    print(f"The result of {x} comes first in the dictionary than {y} is {ToF}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {ToF2}.")
